cdf8e3e8314f173caa281442e95731c85ea0adf6
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-01-19 21:14:20 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8.Subtask6.Problem1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TimeSpan interval = TimeSpan.FromMinutes(30);
+            Console.WriteLine("Введите путь к папке для очистки.");
+            string path = Console.ReadLine();
+            int deletedFiles = 0;
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                long startSize = Getsize(dirInfo);
+                Console.WriteLine($"Исходный размер папки {dirInfo.Name}: {startSize} байт");
+                LookFiles(dirInfo, interval, ref deletedFiles);
+                long newSize = Getsize(dirInfo);
+                Console.WriteLine($"Удалено {deletedFiles} файлов и очищено {startSize - newSize} байт");
+                Console.WriteLine($"Текущий размер папки: {newSize} байт");
+            }
+            else Console.WriteLine("Папка по данному пути не найдена.");
+            Console.ReadLine();
+        }
+
+        static void LookFiles(DirectoryInfo directory, TimeSpan interval, ref int deletedFiles)
+        {
+            FileInfo[] files = directory.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                CheckFile(file, interval, ref deletedFiles);
+            }
+            DirectoryInfo[] dirs = directory.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                CheckDir(dir, interval, ref deletedFiles);
+            }
+        }
+        static void CheckFile(FileInfo file, TimeSpan interval, ref int deletedFiles)
+        {
+            if (DateTime.Now - file.LastAccessTime > interval)
+            {
+                Console.WriteLine($"Файл {file.Name} не использовался более 30 минут и будет удален.");
+                try
+                {
+                    file.Delete();
+                    deletedFiles++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+        static void CheckDir(DirectoryInfo dir, TimeSpan interval, ref int deletedFiles)
+        {
+            if ((DateTime.Now - dir.LastAccessTime > interval) & !IsFilesUsedInInterval(dir, interval))
+            {
+                Console.WriteLine($"Папка {dir.Name} не использовалась более 30 минут и будет удалена.");
+                try
+                {
+                    dir.Delete(true);
+                    deletedFiles++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        static bool IsFilesUsedInInterval(DirectoryInfo dir, TimeSpan interval)
+        {
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (DateTime.Now - dir.LastAccessTime < interval) return true;
+            }
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo directory in dirs)
+            {
+                IsFilesUsedInInterval(directory, interval);
+            }
+            return false;
+        }
+
+        private static long Getsize(DirectoryInfo dirInfo)
+        {
+            long size = 0;
+            FileInfo[] files = dirInfo.GetFiles();
+            try
+            {
+                foreach (FileInfo file in files)
+                {
+                    size += file.Length;
+                }
+                DirectoryInfo[] dirs = dirInfo.GetDirectories();
+                foreach (DirectoryInfo dir in dirs)
+                {
+                    size += Getsize(dir);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return size;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task8.Subtask6.Problem3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task8.Subtask6.Problem3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e5088b8c-a194-4016-8cd9-a9cf15e1db01")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
